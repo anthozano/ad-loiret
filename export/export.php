972a060d8c9072655429b3e1d606d58dd261a9cf
Anthony Lozano <anthony.lozano@anthozano.fr>
2017-11-09 13:04:16 +0100

    Generate a working sql file

--- export/export.php
+++ export/export.php
@@ -1,163 +1,302 @@
 <?php
 
-require "Database.php";
-
-$db = Database::getInstance();
-$pdo = $db->getPDO();
+$mois = [
+  01 => "janvier",
+  02 => "fevrier",
+  03 => "mars",
+  04 => "avril",
+  05 => "mai",
+  06 => "juin",
+  07 => "jullet",
+  08 => "aout",
+  09 => "septembre",
+  10 => "octobre",
+  11 => "novembre",
+  12 => "decembre"
+];
 
 $sql = fopen("../raw/export.sql", 'w');
 
-/*
- * Base
- */
 fwrite($sql,
 "DROP DATABASE IF EXISTS jeanne;
 CREATE DATABASE jeanne
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
+USE jeanne;
 ");
 
-/*
- * Table photo
- */
 fwrite($sql, "
-CREATE TABLE photo(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    ref_cindoc VARCHAR(255) DEFAULT NULL,
-    serie VARCHAR(255) DEFAULT NULL,
-    article VARCHAR(255) DEFAULT NULL,
-    discriminant VARCHAR(255) DEFAULT NULL,
-    notes_bdp VARCHAR(255) DEFAULT NULL,
-    index_personnes VARCHAR(255) DEFAULT NULL,
-    fichier_num VARCHAR(255) DEFAULT NULL,
-    nb_cliche VARCHAR(255) DEFAULT NULL,
-    support VARCHAR(255) DEFAULT NULL,
-    chroma VARCHAR(255) DEFAULT NULL,
-    remarques VARCHAR(255) DEFAULT NULL,
-    id_geo INT DEFAULT NULL,
-    id_description INT DEFAULT NULL,
-    id_sujet INT DEFAULT NULL,
-    id_date INT DEFAULT NULL
-);");
-
-/*
- * Table description
- */
-fwrite($sql, "
-CREATE TABLE description(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    label VARCHAR(255) DEFAULT NULL
-);");
-
-/*
- * Table sujet
- */
-fwrite($sql, "
-CREATE TABLE sujet(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    label VARCHAR(255) DEFAULT NULL
-);");
-
-/*
- * Table l_taille
- */
-fwrite($sql, "
-CREATE TABLE l_taille(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_taille INT DEFAULT NULL,
-    id_photo INT DEFAULT NULL
-);");
-
-/*
- * Table taille
- */
-fwrite($sql, "
-CREATE TABLE taille(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    largeur INT DEFAULT NULL,
-    hauteur INT DEFAULT NULL
-);");
-
-/*
- * Table date
- */
-fwrite($sql, "
-CREATE TABLE date(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    jour INT DEFAULT NULL
-    mois INT DEFAULT NULL
-    annee INT DEFAULT NULL
-);");
-
-/*
- * Table l_iconographie
- */
-fwrite($sql, "
-CREATE TABLE l_iconographie(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_iconographie INT DEFAULT NULL,
-    id_photo INT DEFAULT NULL
-);");
-
-/*
- * Table iconographie
- */
-fwrite($sql, "
-CREATE TABLE iconographie(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    label VARCHAR(255) DEFAULT NULL
-);");
-
-/*
- * Table geo
- */
-fwrite($sql, "
-CREATE TABLE geo(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    ville VARCHAR(255) DEFAULT NULL,
-    coord VARCHAR(255) DEFAULT NULL,
-);");
-
-/*
- * Table coord
- */
-fwrite($sql, "
-CREATE TABLE coord(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    long FLOAT DEFAULT NULL,
-    lati FLOAT DEFAULT NULL,
-);");
-
-
-/*
- * Clef étrangéres
- */
-fwrite($sql, "
-ALTER TABLE photo
-ADD FOREIGN KEY (id_geo) REFERENCES geo(id),
-ADD FOREIGN KEY (id_description) REFERENCES description(id),
-ADD FOREIGN KEY (id_sujet) REFERENCES sujet(id),
-ADD FOREIGN KEY (id_date) REFERENCES date(id);
-
-ALTER TABLE l_iconographie
-ADD FOREIGN KEY (id_iconographie) REFERENCES iconographie(id),
-ADD FOREIGN KEY (id_photo) REFERENCES photo(id);
-
-ALTER TABLE l_taille
-ADD FOREIGN KEY (id_taille) REFERENCES taille(id),
-ADD FOREIGN KEY (id_photo) REFERENCES photo(id);
+-- -----------------------------------------------------
+-- Table `sujet`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sujet` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `label` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`));
 ");
+
+fwrite($sql, "
+-- -----------------------------------------------------
+-- Table `date`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `date` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `jour` INT NULL,
+  `mois` INT NULL,
+  `annee` INT NULL,
+  PRIMARY KEY (`id`));
+");
+
+fwrite($sql, "
+-- -----------------------------------------------------
+-- Table `geo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `geo` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `commune` VARCHAR(255) NULL,
+  `coordonnee` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`));
+");
+
+fwrite($sql, "
+-- -----------------------------------------------------
+-- Table `description`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `description` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `label` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`));
+");
+
+fwrite($sql, "
+-- -----------------------------------------------------
+-- Table `photo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `photo` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `serie` VARCHAR(255) NULL,
+  `article` VARCHAR(255) NULL,
+  `discriminant` VARCHAR(255) NULL,
+  `notes` VARCHAR(255) NULL,
+  `index_personnes` VARCHAR(255) NULL,
+  `fichier_num` VARCHAR(255) NULL,
+  `nb_cliche` VARCHAR(255) NULL,
+  `support` VARCHAR(255) NULL,
+  `chroma` VARCHAR(255) NULL,
+  `remarques` VARCHAR(255) NULL,
+  `id_date` INT NOT NULL,
+  `id_geo` INT NOT NULL,
+  `id_description` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_photo_date_idx` (`id_date` ASC),
+  INDEX `fk_photo_geo2_idx` (`id_geo` ASC),
+  INDEX `fk_photo_description1_idx` (`id_description` ASC),
+  CONSTRAINT `fk_photo_date`
+    FOREIGN KEY (`id_date`)
+    REFERENCES `date` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_photo_geo2`
+    FOREIGN KEY (`id_geo`)
+    REFERENCES `geo` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_photo_description1`
+    FOREIGN KEY (`id_description`)
+    REFERENCES `description` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+");
+
+fwrite($sql, "
+-- -----------------------------------------------------
+-- Table `iconographie`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `iconographie` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `label` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`));
+");
+
+fwrite($sql, "
+-- -----------------------------------------------------
+-- Table `taille`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taille` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `largeur` DOUBLE NULL,
+  `hauteur` DOUBLE NULL,
+  PRIMARY KEY (`id`));
+");
+
+fwrite($sql, "
+-- -----------------------------------------------------
+-- Table `sujets`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sujets` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `photo_id` INT NOT NULL,
+  `sujet_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_sujets_photo1_idx` (`photo_id` ASC),
+  INDEX `fk_sujets_sujet1_idx` (`sujet_id` ASC),
+  CONSTRAINT `fk_sujets_photo1`
+    FOREIGN KEY (`photo_id`)
+    REFERENCES `photo` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_sujets_sujet1`
+    FOREIGN KEY (`sujet_id`)
+    REFERENCES `sujet` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+");
+
+fwrite($sql, "
+-- -----------------------------------------------------
+-- Table `iconographies`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `iconographies` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `iconographie_id` INT NOT NULL,
+  `photo_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_iconographies_iconographie1_idx` (`iconographie_id` ASC),
+  INDEX `fk_iconographies_photo1_idx` (`photo_id` ASC),
+  CONSTRAINT `fk_iconographies_iconographie1`
+    FOREIGN KEY (`iconographie_id`)
+    REFERENCES `iconographie` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_iconographies_photo1`
+    FOREIGN KEY (`photo_id`)
+    REFERENCES `photo` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+");
+
+fwrite($sql, "
+-- -----------------------------------------------------
+-- Table `cindoc`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cindoc` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `label` VARCHAR(255) NULL,
+  `photo_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_cindoc_photo1_idx` (`photo_id` ASC),
+  CONSTRAINT `fk_cindoc_photo1`
+    FOREIGN KEY (`photo_id`)
+    REFERENCES `photo` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+");
+
+fwrite($sql, "
+-- -----------------------------------------------------
+-- Table `tailles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tailles` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `photo_id` INT NOT NULL,
+  `taille_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_tailles_photo1_idx` (`photo_id` ASC),
+  INDEX `fk_tailles_taille1_idx` (`taille_id` ASC),
+  CONSTRAINT `fk_tailles_photo1`
+    FOREIGN KEY (`photo_id`)
+    REFERENCES `photo` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tailles_taille1`
+    FOREIGN KEY (`taille_id`)
+    REFERENCES `taille` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+");
+
+$i = 0;
 
 if (($handle = fopen("../raw/ProjetBDDL3.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 0, ";")) !== FALSE) {
-      
-      fwrite($sql, "
-        INSERT INTO photo(ref_cindoc, serie, article, discriminant, notes_bdp, index_personnes, fichier_num, nb_cliche, support, chroma, remarques)
-        VALUES ($data[0]);
-      ");
 
-      var_dump($data);
-      exit();
+        for ($j = 0; $j < count($data); $j++) {
+            $row[$j] = trim($data[$j]) == "" ? "NULL" : $data[$j];
+        }
+
+        $date = ["day" => "NULL", "month" => "NULL", "year" => "NULL"];
+        $raw_date = explode(':', $data[7]);
+        $row[7] = trim($raw_date[count($raw_date) - 1]);
+
+        if (preg_match('/[0-9]{4}/', $row[7], $year)) {
+          $date["year"] = $year[0];
+        }
+
+        fwrite($sql, "
+INSERT INTO date(id, jour, annee, mois)
+VALUES ($i, " . $date["day"] . ", " . $date["month"] . ", " . $date["year"] . ");
+        ");
+
+        fwrite($sql, "
+INSERT INTO description(id, label)
+VALUES ($i, \"$row[6]\");
+        ");
+
+        fwrite($sql, "
+INSERT INTO geo(id, commune, coordonnee)
+VALUES ($i, \"$row[4]\", NULL);
+        ");
+
+        fwrite($sql, "
+INSERT INTO photo(id, serie, article, discriminant, notes, index_personnes, fichier_num, nb_cliche, support, chroma, remarques, id_description, id_date, id_geo)
+VALUES ($i, \"$row[1]\", \"$row[2]\", \"$row[3]\", \"$row[8]\", \"$row[9]\", \"$row[10]\", \"$row[12]\", \"$row[14]\", \"$row[15]\", \"$row[16]\",
+  $i, $i, $i
+);
+        ");
+
+        fwrite($sql, "
+INSERT INTO sujet(label)
+VALUES (\"$row[5]\");
+        ");
+
+        $taille = ["largeur" => 0, "hauteur" => 0];
+
+        fwrite($sql, "
+INSERT INTO taille(largeur, hauteur)
+VALUES (" . $taille["largeur"] . ", " . $taille["hauteur"] . ");
+        ");
+
+        fwrite($sql, "
+INSERT INTO iconographie(label)
+VALUES (\"$row[6]\");
+        ");
+
+        fwrite($sql, "
+INSERT INTO cindoc(label, photo_id)
+VALUES ($row[0], $i);
+        ");
+
+        fwrite($sql, "
+INSERT INTO sujets(photo_id, sujet_id)
+VALUES ($i, $i);
+        ");
+
+        fwrite($sql, "
+INSERT INTO tailles(photo_id, taille_id)
+VALUES ($i, $i);
+        ");
+
+        fwrite($sql, "
+INSERT INTO iconographies(photo_id, iconographie_id)
+VALUES ($i, $i);
+        ");
+        echo("line $i ok\n");
+        $i++;
+        if ($i > 500) {
+          exit();
+        }
     }
     fclose($handle);
 }
+
+?>
